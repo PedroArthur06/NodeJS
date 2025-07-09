4b8ef1d1b3ba7e6fd263085f638b2a2f5244edf7
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-07-09 16:23:46 -0400

    Add: função para as operações básicas

--- Accounts/index.js
+++ Accounts/index.js
@@ -3,3 +3,21 @@ const chalk = require('chalk')
 const fs = require('fs')
 
 console.log("Projeto iniciado")
+
+operation()
+
+function operation(){
+  inquirer
+  .prompt([
+    {
+    type: 'list',
+    name: 'action',
+    message: 'O que você deseja fazer?',
+    choices: ['Criar uma conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair'],
+    },
+  ]).then((ansewr) => {
+    const action = ansewr['action']
+    console.log(action)
+  })
+  .catch((err) => console.log(err))
+}
