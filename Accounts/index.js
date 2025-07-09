952aeb806d08cef4e13a973be96eaec6c65b055d
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-07-09 16:47:58 -0400

    Add: função para inserir nome da conta

--- Accounts/index.js
+++ Accounts/index.js
@@ -1,29 +1,53 @@
 const inquirer = require('inquirer').default;
-const chalk = require('chalk')
-const fs = require('fs')
+const chalk = require('chalk');
+const fs = require('fs');
 
-console.log("Projeto iniciado")
+console.log("Projeto iniciado");
 
-operation()
+operation();
 
-function operation(){
+function operation() {
   inquirer.prompt([
     {
-    type: 'list',
-    name: 'action',
-    message: 'O que você deseja fazer?',
-    choices: ['Criar uma conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair'],
+      type: 'list',
+      name: 'action',
+      message: 'O que você deseja fazer?',
+      choices: ['Criar uma conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair'],
     },
-  ]).then((ansewr) => {
-    const action = ansewr['action']
-    if(action === 'Criar uma conta'){
-      createAccount()
+  ])
+  .then((answer) => {
+    const action = answer['action'];
+    if (action === 'Criar uma conta') {
+      createAccount();
     }
   })
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
+  }
+
+function createAccount() {
+  console.log('Obrigado por escolher os nossos serviços!');
+  console.log('Defina as opções da sua conta a seguir');
+  
+  buildAccount()
 }
 
-function createAccount(){
-  console.log(chalk.bgGreen.black('Obrigado por escolher os nossos serviços!'))
-  console.log(chalk.green('Defina as opções da sua conta a seguir'))
+function buildAccount(){
+  inquirer.prompt([
+    {
+      name:'accountName',
+      message: 'Insira um nome para a sua conta'
+    }
+  ])
+  .then((answer) => {
+    const accountName = answer['accountName']
+    console.info(accountName)
+
+    if(!fs.existsSync('accounts')){
+      fs.mkdirSync('accounts')
+    }
+  })
+  .catch(err => console.log(err))
 }
+
+
+   
