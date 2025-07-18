1edcc9ec551f05fd9b0c15fb20af952404a426d9
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-07-18 16:11:24 -0400

    Add: acessando parametros pela url

--- Express/paramURL/index.js
+++ Express/paramURL/index.js
@@ -4,6 +4,13 @@ const port = 3000;
 const path = require('path');
 const basePath = path.join(__dirname, 'templates');
 
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(`Buscando usuário pelo ID: ${id}`);
+
+  res.sendFile(`${basePath}/users.html`);
+});
+
 app.get('/', (req, res) => {
   res.sendFile(`${basePath}/index.html`);
 });
