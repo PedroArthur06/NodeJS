a044ba29626b943229e14d9b7684998a3a1e73d5
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-07-18 16:41:08 -0400

    Add: Post de dados via url

--- Express/Post de dados/index.js
+++ Express/Post de dados/index.js
@@ -4,6 +4,20 @@ const port = 3000;
 const path = require('path');
 const basePath = path.join(__dirname, 'templates');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/users/add', (req, res) => {
+  res.sendFile(`${basePath}/userForm.html`);
+});
+
+app.post('/users/save', (req, res) => {
+  const name = req.body.name;   
+  const email = req.body.email;
+  console.log(`Usuário salvo: ${name}, email: ${email}`);
+  res.sendFile(`${basePath}/userForm.html`);
+});
+
 app.get('/users/:id', (req, res) => {
   const id = req.params.id;
   console.log(`Buscando usuário pelo ID: ${id}`);
